2f0a14047d8b614aa7ed959ff2e983f3bc1f8dc2
Ben Pham <ben58630@gmail.com>
2023-08-26 13:58:20 -0700

    Added loading functionality and added icon

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import { ProgressBar } from "react-loader-spinner";
 
 export default function Home() {
 	interface Result {
@@ -12,9 +13,13 @@ export default function Home() {
 	const [searchText, changeSearchText] = useState("");
 	const [results, changeResults] = useState<Result[]>([]);
 	const [timeElapsed, changeTimeElapsed] = useState(0);
-	const [numResults, changeNumResults] = useState(0);
+	const numResults = results.length;
+	const [loading, setLoading] = useState(false);
+	const [firstSearch, setFirstSearch] = useState(true);
 
 	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		setFirstSearch(false);
+		setLoading(true);
 		e.preventDefault();
 		const modified = searchText.replaceAll(" ", "-");
 		// changeSearchText((prev) => prev.replaceAll(" ", "-"));
@@ -24,37 +29,65 @@ export default function Home() {
 		const data = await fetchedData.json();
 		changeResults(data.results);
 		changeTimeElapsed(data.elapsed);
-		changeNumResults(data.numResults);
+		setLoading(false);
 	};
 
 	return (
-		<main className='flex min-h-screen flex-col items-center p-12 md:p-24 space-y-12'>
+		<main className='flex flex-col items-center min-h-screen p-12 md:p-24 space-y-12 w-screen'>
 			<h1 className='text-6xl text-white font-bold text-center'>
 				UCI ICS Search Engine
 			</h1>
 			<form
-				className='w-1/2 flex justify-center bg-zinc-700 rounded-full'
+				className='max-w-6xl flex justify-center bg-zinc-700 rounded-full self-stretch xl:w-[50vw] xl:self-center'
 				onSubmit={handleSubmit}
 			>
-				{/* <FaSearch
-					style={{ color: "white", padding: "1.5rem" }}
-					className='w-20 h-20'
-				/> */}
+				<FaSearch
+					style={{ color: "white" }}
+					className='w-[50px] h-[50px] m-auto pl-3'
+				/>
 				<input
 					type='text'
-					className='text-white p-5 px-10 grow rounded-full text-xl bg-zinc-700 outline-none'
+					className='text-white p-5 rounded-full text-xl outline-none bg-zinc-700 w-full grow'
 					value={searchText}
 					onChange={(event) => changeSearchText(event.target.value)}
 				/>
 			</form>
 
 			<div className='text-white w-full flex-col'>
-				{results.length > 0 && (
+				{loading && (
+					<>
+						<h1 className='text-center text-5xl font-bold'>
+							Getting Results...
+						</h1>
+						<ProgressBar
+							height='180'
+							width='180'
+							ariaLabel='progress-bar-loading'
+							wrapperStyle={{ margin: "auto" }}
+							wrapperClass='progress-bar-wrapper'
+							borderColor='#FFFFFF'
+							barColor='#FFF'
+						/>
+					</>
+				)}
+
+				{firstSearch && (
+					<h1 className='m-auto grow max-w-4xl h-12 text-center text-3xl font-bold'>
+						Type a search term into the bar above to begin!
+					</h1>
+				)}
+
+				{results.length > 0 && !loading && (
 					<h1 className='m-auto grow max-w-4xl h-12'>
 						About {numResults} results ({timeElapsed} seconds)
 					</h1>
 				)}
-				{results.length > 0 ? (
+				{results.length === 0 && !loading && !firstSearch && (
+					<h1 className='m-auto grow max-w-4xl h-12 text-center text-3xl font-bold'>
+						No results found.
+					</h1>
+				)}
+				{results.length > 0 && !loading ? (
 					results.map((result: Result, idx) => {
 						return (
 							<a
